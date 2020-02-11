54ed641b3cdec6d8acc7cbd1c047a16ffc3e2625
zachescalante <zach.escalante@gmail.com>
2020-02-11 14:36:21 -0500

    Adding functionality to upload files

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
 
+UPLOAD_FOLDER = '/mnt/c/Users/NYCZE/Desktop/flask_app/file_uploads'
+
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 from app import routes
