3f3be971abe59053c5c1eb696835ecf81cd5d1b3
zachescalante <zach.escalante@gmail.com>
2020-02-11 14:44:08 -0500

    Adding the secret key

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ UPLOAD_FOLDER = '/mnt/c/Users/NYCZE/Desktop/flask_app/file_uploads'
 
 app = Flask(__name__)
 
+app.secret_key = 'SECRET KEY'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from app import app
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['xlsx', 'csv'])
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -31,5 +31,5 @@ def upload_file():
 			flash('File successfully uploaded')
 			return redirect('/')
 		else:
-			flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+			flash('Allowed file types are xlsx and csv')
 			return redirect(request.url)
