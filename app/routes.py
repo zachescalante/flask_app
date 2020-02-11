b5da2625e1c520874ff97b110a6d2d91b1e9c5b0
zachescalante <zach.escalante@gmail.com>
2020-02-11 16:29:45 -0500

    Adding the functionality to open files and run functions on the data

--- app/routes.py
+++ app/routes.py
@@ -5,31 +5,35 @@ from app import app
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 
-ALLOWED_EXTENSIONS = set(['xlsx', 'csv'])
+ALLOWED_EXTENSIONS = set(['xlsx', 'csv', 'txt'])
 
 def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-	
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def upper_case(var):
+    return str(var.upper())
+    
 @app.route('/')
 def upload_form():
-	return render_template('upload.html')
+    return render_template('upload.html')
 
 @app.route('/', methods=['POST'])
 def upload_file():
-	if request.method == 'POST':
+    if request.method == 'POST':
         # check if the post request has the file part
-		if 'file' not in request.files:
-			flash('No file part')
-			return redirect(request.url)
-		file = request.files['file']
-		if file.filename == '':
-			flash('No file selected for uploading')
-			return redirect(request.url)
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			flash('File successfully uploaded')
-			return redirect('/')
-		else:
-			flash('Allowed file types are xlsx and csv')
-			return redirect(request.url)
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']           
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            print(upper_case(file.read()))
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('File successfully uploaded')
+            return redirect('/')
+        else:
+            flash('Allowed file types are xlsx, txt and csv')
+            return redirect(request.url)
